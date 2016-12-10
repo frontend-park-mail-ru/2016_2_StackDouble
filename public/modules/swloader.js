15cfed80b0e450165f58aa03f6d7edd22d4ac1aa
glowlex <navsl@yandex.ru>
2016-12-11 00:57:53 +0300

    test

--- public/modules/swloader.js
+++ public/modules/swloader.js
@@ -9,8 +9,14 @@
 		// при удачной регистрации имеем объект типа ServiceWorkerRegistration
 		console.log('ServiceWorker registration', registration);
 		// строкой ниже можно прекратить работу serviceWorker’а
-		//registration.unregister();
+		registration.unregister();
 	}).catch(function (err) {
 		throw new Error('ServiceWorker error: ' + err);
 	});
+
+//test
+	navigator.serviceWorker.getRegistrations().then(function(registrations) {
+	 for(let registration of registrations) {
+	  registration.unregister()
+	} })
 })();
