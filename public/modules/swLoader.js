a73e756614738f07df52f6a0351a5e57ae4b1e08
glowlex <navsl@yandex.ru>
2016-12-11 02:45:26 +0300

    sw

--- public/modules/swLoader.js
+++ public/modules/swLoader.js
@@ -1,25 +1,19 @@
 (function () {
 	'use strict';
 
-	if(navigator.serviceWorker){
-		navigator.serviceWorker.getRegistrations().then(function(registrations) {
-		 for(let registration of registrations) {
-		  registration.unregister()
-		} });
-	}
 
 	if (!navigator.serviceWorker) {
 		return;
 	}
-/*	navigator.serviceWorker.register(
+navigator.serviceWorker.register(
 		'/sw.js'
 	).then(function (registration) {
 		// при удачной регистрации имеем объект типа ServiceWorkerRegistration
 		console.log('ServiceWorker registration', registration);
 		// строкой ниже можно прекратить работу serviceWorker’а
-		registration.unregister();
+		//registration.unregister();
 	}).catch(function (err) {
 		throw new Error('ServiceWorker error: ' + err);
-	});*/
+	});
 
 })();
--- public/sw.js
+++ public/sw.js
@@ -87,11 +87,3 @@ this.addEventListener('activate', function(event) {
     })
   );
 });
-
-
-if(navigator.serviceWorker){
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-   for(let registration of registrations) {
-    registration.unregister()
-  } });
-}
