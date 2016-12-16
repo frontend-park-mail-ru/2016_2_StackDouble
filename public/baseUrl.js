c1ba843346f912adbd4713d81342cd2beff72370
glowlex <navsl@yandex.ru>
2016-12-16 14:44:57 +0400

    fix

--- public/baseUrl.js
+++
@@ -1,7 +0,0 @@
-(function () {
-	'use strict';
-
-	window.baseUrlApp = (window.location.host === 'stack-double.herokuapp.com') ?
-		'https://card-game-09-2016.herokuapp.com' :
-		'https://cardgame092016.herokuapp.com';
-}());
